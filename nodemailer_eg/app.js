101ed59c9fbc911e7341d98a01412c6823df095a
Basil <basiltalias92@gmail.com>
2017-01-27 20:57:52 -0500

    Added more files

--- nodemailer_eg/app.js
+++ nodemailer_eg/app.js
@@ -9,6 +9,7 @@ var nodemailer = require('nodemailer');
 var index = require('./routes/index');
 //var users = require('./routes/users');
 var contact = require('./routes/contact')
+
 var app = express();
 
 // view engine setup
@@ -25,8 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 //app.use('/users', users);
+
 app.use('/contact',contact)
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
--- nodemailer_eg/routes/index.js
+++ nodemailer_eg/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Test' });
 });
+
+router.post('/send',function(req,res,next){
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'email',
+      pass: 'password'
+    }
+  })
+  var mainOptions = {
+     from: 'email',
+     to: 'email',
+     subject: 'Website Submission',
+     text: 'You have new '+req.body.name+'Email'+req.body.email+'Message'+req.body.message
+
+  };
+  transporter.sendMail(mailOptions,function(error,info){
+    if(error){
+      console.log(error)
+    }
+  });
+});
+
 
 module.exports = router;
